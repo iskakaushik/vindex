9e32914fb7875a91b74cf74e1a8f2661858ba588
Kaushik Iska <iska.kaushik@gmail.com>
2020-10-11 13:43:49 -0700

    use leveldb and clang format

--- vindex_main.cpp
+++ vindex_main.cpp
@@ -1,9 +1,29 @@
-#include "leveldb/db.h"
 #include <iostream>
 
-int main() {
-  leveldb::DB *db;
+#include "leveldb/db.h"
 
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+int main() {
+  leveldb::DB* db;
+
+  leveldb::Options options;
+  options.create_if_missing = true;
+
+  leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
+  if (!status.ok()) {
+    std::cerr << "Unable to create / open the database";
+    return EXIT_FAILURE;
+  }
+
+  leveldb::WriteOptions writeOptions;
+  db->Put(writeOptions, "key1", "val1");
+
+  // Iterate over each item in the database and print them
+  leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+
+  for (it->SeekToFirst(); it->Valid(); it->Next()) {
+    std::cout << it->key().ToString() << " : " << it->value().ToString()
+              << std::endl;
+  }
+
+  return EXIT_SUCCESS;
 }
